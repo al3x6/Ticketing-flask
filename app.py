3122b53ed91586810ecf90cb8cc1877ffff8d0b5
al3x6 <alexisar2002@gmail.com>
2025-02-13 11:20:45 +0100

    Gestion des erreurs

--- app.py
+++ app.py
@@ -7,5 +7,13 @@ app = Flask(__name__)
 def home():
     return render_template('index.html', message="Bienvenue sur Flask !")
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("404.html"), 404
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return render_template("500.html"), 500
+
 if __name__ == '__main__':
     app.run(debug=True)
